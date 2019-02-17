fb735bfba1e1dc60abd0adad20eb005cbaea12a6
c1845682 <BenedekDJ@cardiff.ac.uk>
2019-02-17 16:44:30 +0000

    Check Style Corrections before merging into master

--- src/main/java/com/nsa/cm6123/assessment/monopoly/Dice.java
+++ src/main/java/com/nsa/cm6123/assessment/monopoly/Dice.java
@@ -10,19 +10,26 @@ public class Dice {
 
     //Creating a new instance of the Random class for the dice.
     private static Random dice = new Random();
-    
-    public static int diceRoll(){
+    private static final int MAX_DICE_ROLL = 6;
+
+    private Dice() {
+
+    }
+
+    public static int diceRoll() {
 
         /**
-         * As Monopoly rules involves the use of 2 dices, that is what I implementing 
-         * here with the use of random numbers and returning the final result. 
+         * As Monopoly rules involves the use of 2 dices,
+         * that is what I implementing
+         * here with the use of random numbers and returning
+         * the final result.
          */
 
         int rollDice1;
         int rollDice2;
-        rollDice1 = dice.nextInt(6);
-        rollDice2 = dice.nextInt(6);
-        int finalDice = rollDice1 + rollDice2; 
+        rollDice1 = dice.nextInt(MAX_DICE_ROLL) + 1;
+        rollDice2 = dice.nextInt(MAX_DICE_ROLL) + 1;
+        int finalDice = rollDice1 + rollDice2;
         return finalDice;
     }
     }
--- src/test/java/com/nsa/cm6123/assessment/monopoly/DiceTest.java
+++ src/test/java/com/nsa/cm6123/assessment/monopoly/DiceTest.java
@@ -1,29 +1,21 @@
 package com.nsa.cm6123.assessment.monopoly;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class DiceTest {
 
-    private Dice dice;
-
-    @Before
-    public void setUp(){
-        dice = new Dice();
-    }
-
     @Test
     public void diceRollNotMoreThan13() {
 
-        assertTrue(dice.diceRoll() < 13);
+        assertTrue(Dice.diceRoll() < 13);
     }
 
     @Test
     public void diceIsNotANegativeNumber() {
 
-        assertFalse(dice.diceRoll() <= 1);
+        assertFalse(Dice.diceRoll() <= 1);
 
     }
 }
